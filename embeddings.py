1a16143bf4a1b7c69a49fe22314c285f62d1e153
hwngai <61653571+hwngai@users.noreply.github.com>
2024-01-22 15:25:31 +0700

    .

--- embeddings.py
+++ embeddings.py
@@ -3,9 +3,12 @@ from langchain.vectorstores import FAISS
 import torch
 import uuid
 import os
-
+import nest_asyncio
+import time
 from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.document_loaders import TextLoader
+from langchain.document_loaders import WebBaseLoader, NewsURLLoader, SeleniumURLLoader
+
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -49,5 +52,26 @@ def retriever_1(query_texts, chunks, n_results):
     results = retriever.get_relevant_documents(query_texts)
     return results
 
+def loading_webpages(website_urls, loader_class = NewsURLLoader):
+    nest_asyncio.apply()
+    loader = loader_class(website_urls)
+    # loader.requests_per_second = 1
+    docs = loader.load()
+    return docs
 
 
+def create_index_and_retriever(chunks, embeddings):
+    index = FAISS.from_documents(chunks, embeddings)
+    retriever = index.as_retriever()
+    return retriever
+
+
+
+urls = ["https://tamanhhospital.vn/dau-bung/", "https://www.vinmec.com/vi/tieu-hoa-gan-mat/thong-tin-suc-khoe/vi-tri-dau-bung-canh-bao-benh-gi/", "https://hongngochospital.vn/dau-bung-canh-bao-nhieu-benh-ly-nguy-hiem/", "https://www.msdmanuals.com/vi-vn/chuy%C3%AAn-gia/r%E1%BB%91i-lo%E1%BA%A1n-ti%C3%AAu-h%C3%B3a/b%E1%BB%A5ng-c%E1%BA%A5p-v%C3%A0-ph%E1%BA%ABu-thu%E1%BA%ADt-ti%C3%AAu-h%C3%B3a/%C4%91au-b%E1%BB%A5ng-c%E1%BA%A5p-t%C3%ADnh"]
+start_time = time.time()
+docs = loading_webpages(website_urls = urls)
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"time {urls}: {elapsed_time:.2f} giây")
+
+print("docs: ", docs)
